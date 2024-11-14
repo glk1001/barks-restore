7b4c87ccf4a1ccc877949498cc5b23a0a5cfdeb9
glk1001 <gregg.kay@gmail.com>
2024-11-14 17:22:32 +1100

    Made smooth clearer.

--- experiments/smooth_image.py
+++ experiments/smooth_image.py
@@ -2,19 +2,35 @@ import gmic
 
 
 def smooth_image_file(in_file: str, out_file: str):
-    # TODO: Explain or constify these params.
-    smooth_params = '420,0.5,0.6,2.5,5.0,0.3,60.0,0.94,0,1,2,0,"0",nan,nan'
+    amplitude = 420
+    sharpness = 0.5
+    anisotropy = 0.6
+    alpha = 2.5
+    sigma = 5.0
+    dl = 0.8
+    da = 30
+    precision = 2
+    interpolation = 0 # nearest
+    fast_approx = 1
+    repeat = 2
+    channels = 0
+    smooth_params = (f'{amplitude},'
+                     f'{sharpness},'
+                     f'{anisotropy},'
+                     f'{alpha},'
+                     f'{sigma},'
+                     f'{dl},'
+                     f'{da},'
+                     f'{precision},'
+                     f'{interpolation},'
+                     f'{fast_approx},'
+                     f'{repeat},'
+                     f'{channels}')
+
+    # print(smooth_params)
     gmic.run(
         f'"{in_file}"'
-        f" -repeat 5 fx_smooth_anisotropic {smooth_params} -done"
+        f" fx_smooth_anisotropic {smooth_params}"
         f" -threshold[-1] 100,1 normalize[-1] 0,255"
         f' -output[-1] "{out_file}"'
-    )  # smooth latest - very good
-
-    # gmic.run(
-    #     f'"{in_file}" -repeat 5 -smooth 10,0,1,1,2 -done -output[-1] "{out_file}"'
-    # )  # smooth0
-    # gmic.run(f'"{in_file}" smooth 10,0,1,1,2 output "{out_file}"') # smooth0
-    # gmic.run(f'"{in_file}" smooth 50,0,1,1,2 output "{out_file}"') # smooth1
-    # gmic.run(f'"{in_file}" fx_smooth_median 8,255,0,"0",nan,nan output "{out_file}"')
-    # gmic.run(f'"{in_file}" -fx_smooth_anisotropic "80","0.5","0.3","2","5.1","0.8","70.81","2","2","1","1","0","0","50","50" output "{out_file}"')
+    )
