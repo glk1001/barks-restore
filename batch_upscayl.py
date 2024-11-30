b9d969b08d99d3c68f84d4f7f2ede735de5dde7a
glk1001 <gregg.kay@gmail.com>
2024-11-30 18:02:56 +1100

    Changed batch upscayl to use Fantagraphics directories.

--- batch_upscayl.py
+++ batch_upscayl.py
@@ -1,38 +1,85 @@
+import argparse
 import os
-import sys
-from pathlib import Path
 
+from intspan import intspan
+
+from barks_fantagraphics.comics_database import (
+    ComicsDatabase,
+    PageType,
+    get_default_comics_database_dir,
+)
+from barks_fantagraphics.comics_info import JPG_FILE_EXT, PNG_FILE_EXT
+from barks_fantagraphics.comics_utils import get_story_files_of_page_type
 from experiments.upscale_image import upscale_image_file
 
-if __name__ == "__main__":
-    scale = 4
+RESTORABLE_PAGE_TYPES = [
+    PageType.BODY,
+    PageType.FRONT_MATTER,
+    PageType.BACK_MATTER,
+]
 
-    input_image_dir_and_stem = sys.argv[1]
-    input_image_range_str = sys.argv[2]
-    out_dir = sys.argv[3]
+COMICS_DATABASE_DIR_ARG = "--comics-database-dir"
+VOLUME_ARG = "--volume"
 
-    if not os.path.isdir(out_dir):
-        print(f'ERROR: Can\'t find output directory: "{out_dir}".')
-        sys.exit(1)
+SCALE = 4
 
-    input_image_range = input_image_range_str.split("-")
-    assert 1 <= len(input_image_range) <= 2
-    if len(input_image_range) == 1:
-        input_image_range.append(input_image_range[0])
 
-    for n in range(int(input_image_range[0]), int(input_image_range[1]) + 1):
-        input_image_file = f"{input_image_dir_and_stem}-{n:03}.jpg"
-        if not os.path.exists(input_image_file):
-            print(f'ERROR: Can\'t find input image: "{input_image_file}".')
-            sys.exit(1)
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Upscayl jpg images in specified Fantagraphics volumes."
+    )
 
-        input_image_stem = Path(input_image_dir_and_stem).stem
-        output_upscayl_file = os.path.join(
-            out_dir, f"{input_image_stem}-upscayl-x{scale}-{n:03}.png"
-        )
+    parser.add_argument(
+        COMICS_DATABASE_DIR_ARG,
+        action="store",
+        type=str,
+        default=get_default_comics_database_dir(),
+    )
+    parser.add_argument(
+        VOLUME_ARG,
+        action="store",
+        type=str,
+        required=True,
+    )
 
-        if os.path.exists(output_upscayl_file):
-            print(f'ERROR: Can\'t overwrite target file: "{output_upscayl_file}".')
-            sys.exit(1)
+    args = parser.parse_args()
 
-        upscale_image_file(input_image_file, output_upscayl_file, scale)
+    return args
+
+
+def upscale(in_file: str, out_file: str) -> None:
+    if not os.path.isfile(in_file):
+        raise Exception(f'Could not find srce file: "{in_file}".')
+    if os.path.isfile(out_file):
+        print(f'Dest file exists - skipping: "{out_file}".')
+        return
+
+    print(f'Upscayling srce file "{in_file}" to dest "{out_file}".')
+    upscale_image_file(in_file, out_file, SCALE)
+
+
+cmd_args = get_args()
+comics_database = ComicsDatabase(cmd_args.comics_database_dir)
+vol_list = list(intspan(cmd_args.volume))
+
+titles = comics_database.get_all_story_titles_in_fantagraphics_volume(vol_list)
+
+srce_file_list = []
+dest_file_list = []
+for title in titles:
+    comic_book = comics_database.get_comic_book(title)
+
+    srce_dir = comic_book.get_srce_image_dir()
+    srce_files = get_story_files_of_page_type(
+        srce_dir, comic_book, JPG_FILE_EXT, RESTORABLE_PAGE_TYPES
+    )
+    srce_file_list.extend(srce_files)
+
+    dest_dir = comic_book.get_srce_upscayled_image_dir()
+    dest_files = get_story_files_of_page_type(
+        dest_dir, comic_book, PNG_FILE_EXT, RESTORABLE_PAGE_TYPES
+    )
+    dest_file_list.extend(dest_files)
+
+for srce_file, dest_file in zip(srce_file_list, dest_file_list):
+    upscale(srce_file, dest_file)
